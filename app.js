81650aa942c19513b2aa04d5bf5cbae162ad0c54
hta393939 <46343954+hta393939@users.noreply.github.com>
2021-08-13 19:36:27 +0900

    Update app.js
    brower editor test

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+/**
+ * @file app.js
+ */
+
+(function(_global) {
+    
+class App {
+    constructor() {
+        this.cl = this.constructor.name;
+    }
+    async init() {
+    }
+}
+    
+if (false) {
+} else {
+    _global.App = App;
+}
+    
+})( (this || 0).self || (typeof self !== 'undefined' ? self : global) );
 
